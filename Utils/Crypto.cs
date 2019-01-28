b9d8fe772bb6a0673b50222ca5dde4ccfe2ebc65
Justus Hertel <justus.hertel@live.de>
2019-01-28 22:17:36 +0100

    Initial Code Commit featuring WebJsonHelper and Crypto classes

---
+++ Utils/Crypto.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Utils
+{
+    public static class Crypto
+    {
+        public static (string Hash, string Salt) Encrypt(string plainText, string password)
+        {
+            string e = null;
+            string salt = null;
+            RijndaelManaged aes = null;
+
+            try
+            {
+                salt = genSalt();
+                var key = new Rfc2898DeriveBytes(password, Convert.FromBase64String(salt));
+
+                aes = new RijndaelManaged();
+                aes.Key = key.GetBytes(aes.KeySize / 8);
+                aes.IV = genIV();
+
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(BitConverter.GetBytes(aes.IV.Length), 0, sizeof(int));
+                    memoryStream.Write(aes.IV, 0, aes.IV.Length);
+
+                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var streamWriter = new StreamWriter(cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                    }
+
+                    e = Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+            finally
+            {
+                aes?.Clear();
+            }
+
+            return (e, salt);
+        }
+
+        public static string Decrypt(string cipherText, string password, string salt)
+        {
+            string d = null;
+            RijndaelManaged aes = null;
+
+            try
+            {
+                var key = new Rfc2898DeriveBytes(password, Convert.FromBase64String(salt));
+
+                var bytes = Convert.FromBase64String(cipherText);
+
+                using (var memoryStream = new MemoryStream(bytes))
+                {
+                    aes = new RijndaelManaged();
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = readByteArray(memoryStream);
+
+                    var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var streamReader = new StreamReader(cryptoStream))
+                        {
+                            d = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                aes?.Clear();
+            }
+
+            return d;
+        }
+
+        #region Helper methods
+
+        private static byte[] readByteArray(Stream s)
+        {
+            var rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+                throw new SystemException("Stream did not contain properly formatted byte array");
+
+            var buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+            if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+                throw new SystemException("Did not read byte array properly");
+
+            return buffer;
+        }
+
+        private static string genSalt()
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var salt = new byte[32];
+            rng.GetBytes(salt);
+            return Convert.ToBase64String(salt);
+        }
+
+        private static byte[] genIV()
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var IV = new byte[16];
+            rng.GetBytes(IV);
+            return IV;
+        }
+
+        #endregion
+    }
+}
---
+++ Utils/WebJsonHelper.cs
@@ -0,0 +1,140 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Utils
+{
+    public static class WebJsonHelper
+    {
+        #region Requests
+
+        internal static HttpWebRequest CreateGetRequest(string Url)
+        {
+            var webRequest = (HttpWebRequest) WebRequest.Create(Url);
+            webRequest.Method = WebRequestMethods.Http.Get;
+            webRequest.Accept = "application/json";
+            return webRequest;
+        }
+
+        internal static HttpWebRequest CreatePutRequest(string Url)
+        {
+            var webRequest = (HttpWebRequest) WebRequest.Create(Url);
+            webRequest.Method = WebRequestMethods.Http.Put;
+            webRequest.ContentType = "application/json";
+            return webRequest;
+        }
+
+        #endregion
+
+        #region Get
+
+        #region GetRawJson
+
+        public static async Task<string> GetRawJson(string Url)
+        {
+            var httpWebRequest = CreateGetRequest(Url);
+
+            return new StreamReader((await httpWebRequest.GetResponseAsync()).GetResponseStream()).ReadToEnd();
+        }
+
+        public static async Task<string> GetRawJson(string Url, string Proxy, int Port)
+        {
+            var httpWebRequest = CreateGetRequest(Url);
+
+            httpWebRequest.Proxy = new WebProxy(Proxy, Port);
+
+            return new StreamReader((await httpWebRequest.GetResponseAsync()).GetResponseStream()).ReadToEnd();
+        }
+
+        public static async Task<string> GetRawJson(string Url, string Proxy, int Port, NetworkCredential Credentials)
+        {
+            var httpWebRequest = CreateGetRequest(Url);
+
+            httpWebRequest.Proxy = new WebProxy(Proxy, Port) {Credentials = Credentials};
+
+            return new StreamReader((await httpWebRequest.GetResponseAsync()).GetResponseStream()).ReadToEnd();
+        }
+
+        #endregion
+
+        #region GetDeserialized
+
+        public static async Task<T> GetDeserialized<T>(string Url)
+            => JsonConvert.DeserializeObject<T>(await GetRawJson(Url));
+
+        public static async Task<T> GetDeserialized<T>(string Url, string Proxy, int Port)
+            => JsonConvert.DeserializeObject<T>(await GetRawJson(Url, Proxy, Port));
+
+
+        public static async Task<T> GetDeserialized<T>(string Url, string Proxy, int Port,
+            NetworkCredential Credentials)
+            => JsonConvert.DeserializeObject<T>(await GetRawJson(Url, Proxy, Port, Credentials));
+
+        #endregion
+
+        #endregion
+
+        #region Put
+
+        #region PutRawJson
+
+        public static async void PutRawJson(string Json, string Url)
+        {
+            var httpWebRequest = CreatePutRequest(Url);
+
+            var requestStream = httpWebRequest.GetRequestStream();
+            await requestStream.WriteAsync(Encoding.ASCII.GetBytes(Json), 0, Json.Length);
+            requestStream.Close();
+            await httpWebRequest.GetResponseAsync();
+        }
+
+        public static async void PutRawJson(string Json, string Url, string Proxy, int Port)
+        {
+            var httpWebRequest = CreatePutRequest(Url);
+
+            httpWebRequest.Proxy = new WebProxy(Proxy, Port);
+
+            var requestStream = httpWebRequest.GetRequestStream();
+            await requestStream.WriteAsync(Encoding.ASCII.GetBytes(Json), 0, Json.Length);
+            requestStream.Close();
+            await httpWebRequest.GetResponseAsync();
+        }
+
+        public static async void PutRawJson(string Json, string Url, string Proxy, int Port,
+            NetworkCredential Credentials)
+        {
+            var httpWebRequest = CreatePutRequest(Url);
+
+            httpWebRequest.Proxy = new WebProxy(Proxy, Port) {Credentials = Credentials};
+
+            using (var requestStream = httpWebRequest.GetRequestStream())
+            {
+                await requestStream.WriteAsync(Encoding.ASCII.GetBytes(Json), 0, Json.Length);
+            }
+
+            await httpWebRequest.GetResponseAsync();
+        }
+
+        #endregion
+
+        #region PutObject
+
+        public static async void PutObject(object @Object, string Url)
+            => await Task.Run(() => PutRawJson(JsonConvert.SerializeObject(@Object, Formatting.Indented), Url));
+
+        public static async void PutObject(object @Object, string Url, string Proxy, int Port)
+            => await Task.Run(() =>
+                PutRawJson(JsonConvert.SerializeObject(@Object, Formatting.Indented), Url, Proxy, Port));
+
+        public static async void PutObject(object @Object, string Url, string Proxy, int Port,
+            NetworkCredential Credentials)
+            => await Task.Run(() => PutRawJson(JsonConvert.SerializeObject(@Object, Formatting.Indented), Url, Proxy,
+                Port, Credentials));
+
+        #endregion
+
+        #endregion
+    }
+}
